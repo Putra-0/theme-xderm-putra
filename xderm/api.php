81124b1cfd32061b06a6f732a14f05bac8890967
Adi Putra <73918462+Putra-0@users.noreply.github.com>
2021-11-19 04:03:33 +0700

    Add files via upload

--- xderm/api.php
+++ xderm/api.php
@@ -1,6 +1,21 @@
 <?php
 
+$tx = shell_exec("ifconfig tun0 | grep 'bytes:' | awk '{print $7, $8}' | sed 's/(//g; s/)//g'");
+$rx= shell_exec("ifconfig tun0 | grep 'bytes:' | awk '{print $3, $4}' | sed 's/(//g; s/)//g'");
+
+echo "<table align='center' style='margin-top:-20px;margin-bottom:10px;'>
+	<tr class='col-butt'>
+		<td style='width:370px;padding-top:5px' class='glow-on-hover'><span style='color:red'>RX | TX: </span><span>$rx | $tx</span</td>
+	</tr>
+      </table>"
+
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<link type="text/css" rel="stylesheet" href="css/style.css">
+</head>
+
 
 <script>
 $.get("http://ip-api.com/json", function (response) {
